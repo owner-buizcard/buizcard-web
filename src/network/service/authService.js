a49e19f3ca51f7e733bad44b04ae0a6017421b2f
dhanasra <dhana.spiderlingz@gmail.com>
2023-12-17 20:34:33 +0530

    update: linkedin-auth

--- src/network/service/authService.js
+++ src/network/service/authService.js
@@ -17,5 +17,5 @@ export async function loginWithGithub(data){
 }
 
 export async function loginWithLinkedin(data){
-    window.location.href = 'https://x9a0br47t1.execute-api.us-east-1.amazonaws.com/dev/auth/linkedin';
+    window.location.href = 'https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=864hy6jn3uyw75&redirect_uri=https://x9a0br47t1.execute-api.us-east-1.amazonaws.com/dev/auth/linkedin/callback&state=foobar&scope=profile%20openid%20email';
 }
