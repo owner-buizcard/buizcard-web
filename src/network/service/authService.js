a1bf9b966fc5847a047d580835e2a3f1a2fc1d76
dhanasra <dhana.spiderlingz@gmail.com>
2024-01-30 11:50:07 +0530

    update

--- src/network/service/authService.js
+++ src/network/service/authService.js
@@ -17,7 +17,7 @@ export async function loginWithGithub(data){
 }
 
 export async function loginWithLinkedin(data){
-    window.location.href = 'https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=864hy6jn3uyw75&redirect_uri=https://x9a0br47t1.execute-api.us-east-1.amazonaws.com/dev/auth/linkedin/callback&state=foobar&scope=profile%20openid%20email';
+    window.location.href = 'https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=864hy6jn3uyw75&redirect_uri=https://x9a0br47t1.execute-api.us-east-1.amazonaws.com/dev/auth/callback/linkedin&state=foobar&scope=profile%20openid%20email';
 }
 
 export async function forgotPassword(data){
