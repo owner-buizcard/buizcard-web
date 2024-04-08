80fc1902828c8a7561fffc1b37bc5e93d9c2cffa
dhanasra <dhana.spiderlingz@gmail.com>
2024-04-08 20:31:31 +0530

    update: banner responsive

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -1,46 +1,63 @@
 import { ArrowRightOutlined } from "@ant-design/icons";
-import { Box, Button, Grid, Stack, Typography } from "@mui/material";
+import { Box, Button, Grid, Stack, Typography, useMediaQuery } from "@mui/material";
 import AnimateButton from "../../components/@extended/AnimateButton";
 import { useNavigate } from "react-router-dom";
+import { useTheme } from "@emotion/react";
 
 const Banner = ()=>{
 
   const navigate = useNavigate();
+  const theme = useTheme();
+
+  const isMdScreen = useMediaQuery('(min-width:960px)');
+  const isMobileScreen = useMediaQuery('(min-width:600px)');
 
   return (
     <Grid container sx={{py: "200px"}}>
-      <Grid item xs={2}/>
-      <Grid item xs={8} >
+      <Grid item xs={12} sx={{justifyContent: "center", display: "flex"}}>
         <Typography 
           sx={{
             background: `linear-gradient(45deg, #DE6262 30%, rgba(79, 75, 255, 0.9294) 90%)`,
             WebkitBackgroundClip: 'text',
             backgroundClip: 'text',
             color: 'transparent',
-            fontSize: "54px",
+            fontSize: "32px",
             lineHeight: 1.2,
+            margin: "0 16px 0 16px",
             fontWeight: 800,
-            textAlign: "center"
+            maxWidth: "1000px",
+            textAlign: "center",
+            [theme.breakpoints.up('md')]: {
+              fontSize: "54px",
+            },
+            [theme.breakpoints.up('sm')]: {
+              fontSize: "44px",
+            }
           }}  
-        >{`Grow faster with Popl, the #1 Digital Business Card Platform`}</Typography>
+        >{`Elevate Networking With Ai-Powered Business Cards`}</Typography>
       </Grid>  
-      <Grid item xs={2}/>
 
       <Grid item xs={12}>
         <Box height={"24px"}/>
       </Grid>
 
-      <Grid item xs={3}/>
-      <Grid item xs={6}>
-        <Typography
+      <Grid item xs={12} sx={{justifyContent: "center", display: "flex"}}>
+        <Typography 
           sx={{
             textAlign: "center",
             fontWeight: 500,
-            fontSize: "18px"
+            fontSize: "18px",
+            margin: "0 36px 0 36px",
+            maxWidth: "700px",
+            [theme.breakpoints.up('md')]: {
+              fontSize: "18px",
+            },
+            [theme.breakpoints.up('sm')]: {
+              fontSize: "18px",
+            }
           }}  
-        >Easily create digital business cards for yourself or your team. Use them to make connections that grow your business.</Typography>
-      </Grid>
-      <Grid item xs={3}/>
+        >{`Easily create digital business cards for yourself or your team. Use them to make connections that grow your business.`}</Typography>
+      </Grid> 
 
       <Grid item xs={12}>
         <Box height={"44px"}/>
@@ -57,12 +74,22 @@ const Banner = ()=>{
               borderRadius: "30px",
               padding: "16px 64px",
               cursor: "pointer",
-              boxShadow: "0px 4px 14px 0px #DE6262"
+              boxShadow: "0px 4px 14px 0px #DE6262",
+              [theme.breakpoints.down('sm')]: {
+                padding: "14px 24px",
+              }
             }}
           >
             <Stack direction={"row"} alignItems={"center"} spacing={2}>
-              <Typography variant="h4">Create Your Card</Typography>
-              <ArrowRightOutlined style={{fontSize: "20px"}}/>
+              <Typography variant="h4"
+                sx={{
+                  fontSize: "18px",
+                  [theme.breakpoints.down('sm')]: {
+                    fontSize: "16px",
+                  },
+                }}
+              >Create Your Card</Typography>
+              <ArrowRightOutlined style={{fontSize: !isMobileScreen ? "16px" :"20px"}}/>
             </Stack>
           </Button>
         </AnimateButton>
