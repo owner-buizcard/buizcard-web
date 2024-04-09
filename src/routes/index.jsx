c3e6b8825d97265553b3146d7fef9cc809f83a2c
dhanasra <dhana.spiderlingz@gmail.com>
2024-04-10 00:29:00 +0530

    update: routing

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -7,66 +7,56 @@ import { useEffect } from 'react';
 import { checkCookies } from '../utils/utils';
 import Cookies from 'js-cookie';
 
-
 const CheckAuthAndStorage = ({ children }) => {
   const navigate = useNavigate();
   const user = useSelector((state) => state.app.user);
   const config = useSelector((state) => state.app.configs);
   const currentLocation = useLocation();
 
-  console.log("dhana")
-
   useEffect(() => {
-    console.log('dahandjddjjddjdjdj')
-      
-      const isLoggedIn = checkCookies();
+    const isLoggedIn = checkCookies();
+    const redirect = Cookies.get('redirect');
+    const isUnAuthRoute = ['/', '/login', '/register', '/auth/callback', '/password/forgot', '/password/reset', '/verify-email', '/check-mail', '/legal/privacy', '/legal/terms'].includes(redirect ?? currentLocation.pathname);
+    const routePattern = /^\/app\/p\/card\/\w+$/; 
+    const matchesRedirect = redirect && routePattern.test(redirect);
+    const matchesCurrentLocation = routePattern.test(currentLocation.pathname);
+    const isConfigRoute = matchesRedirect || matchesCurrentLocation;
+    const hasLocalStorage = user !== null;
+    const hasConfig = config !== null;
 
+    if (currentLocation.pathname === '/loading' || currentLocation.pathname.includes('/i/')) {
+      return;
+    }
 
-      const redirect = Cookies.get('redirect');
+    if(isLoggedIn && !hasLocalStorage){
+      navigate('/loading');
+    }
 
-      const isUnAuthRoute = ['/', '/login', '/register', '/auth/callback', '/password/forgot', '/password/reset', '/verify-email', '/check-mail', '/legal/privacy', '/legal/terms'].includes(redirect ?? currentLocation.pathname);
+    if(isLoggedIn && isUnAuthRoute && currentLocation.pathname !== '/dashboard'){
+      navigate('/dashboard');
+    }
 
-      const routePattern = /^\/app\/p\/card\/\w+$/; 
-      const matchesRedirect = redirect && routePattern.test(redirect);
-      const matchesCurrentLocation = routePattern.test(currentLocation.pathname);
-      const isConfigRoute = matchesRedirect || matchesCurrentLocation;
+    // navigate('/loading');
 
-      const hasLocalStorage = user !== null;
-      const hasConfig = config !== null;
+    // if (isLoggedIn) {
+    //   if (!hasLocalStorage) {
+    //     Cookies.set('redirect', currentLocation.pathname);
+    //     navigate('/loading');
+    //   } else if (isUnAuthRoute && currentLocation.pathname !== '/dashboard') {
+    //     navigate('/dashboard');
+    //   }
+    // } else {
+    //   if (!isUnAuthRoute && isConfigRoute && !hasConfig) {
+    //     Cookies.set('redirect', currentLocation.pathname);
+    //     navigate('/loading');
+    //   } else if (!isUnAuthRoute) {
+    //     navigate('/login');
+    //   }
+    // }
 
-      if(currentLocation.pathname=='/loading' || currentLocation.pathname.includes('/i/')){
-        return;
-      }
-
-      console.log(`local storage: ${hasLocalStorage}`)
-      console.log(`config: ${hasConfig}`)
-
-      if(isLoggedIn){
-
-        if(!hasLocalStorage){
-          Cookies.set('redirect', currentLocation.pathname);
-          navigate('/loading');
-        }
-
-        if(isUnAuthRoute && currentLocation.pathname!='/dashboard'){
-          navigate('/dashboard');
-        }
-        
-      }else{
-        if(!isUnAuthRoute && isConfigRoute){
-          if(!hasConfig){
-            Cookies.set('redirect', currentLocation.pathname);
-            navigate('/loading');
-          }
-        }else if(!isUnAuthRoute){
-          navigate('/login')
-        }
-      }
-
-      if(redirect){
-        navigate(redirect);
-      }
-
+    // if (redirect) {
+    //   navigate(redirect);
+    // }
   }, [user, navigate, currentLocation, config]);
 
   return <>{children}</>;
@@ -75,9 +65,7 @@ const CheckAuthAndStorage = ({ children }) => {
 export default function ThemeRoutes() {
   return (
     <CheckAuthAndStorage>
-      {
-        useRoutes([CommonRoutes, MainRoutes, LoginRoutes])
-      }
+      {useRoutes([CommonRoutes, MainRoutes, LoginRoutes])}
     </CheckAuthAndStorage>
-  )
+  );
 }
